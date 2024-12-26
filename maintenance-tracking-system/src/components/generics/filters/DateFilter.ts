c330f6c388a2cfbeab3262edfe8e9b277a5f2428
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-12-25 21:11:40 -0800

    Create DateFilter.ts
    Created a filter for the tanstack table. This should replace the filterFn in the column with dates.

---
+++ maintenance-tracking-system/src/components/generics/filters/DateFilter.ts
@@ -0,0 +1,24 @@
+import { FilterFn, Row } from "@tanstack/react-table";
+
+const dateFilter: FilterFn<any> = (row: Row<any>, columnId: string, filterValue: any) => {
+    const emptyStartDate: Boolean = (filterValue[0] == '' || filterValue[0] == null);
+    const emptyEndDate:   Boolean = (filterValue[1] == '' || filterValue[1] == null);
+
+    if (emptyStartDate && emptyEndDate) {
+        return true;
+    }
+
+    const columnDate: string = (row.getValue(columnId) as Date).toISOString().substring(0,10);
+
+    if (!emptyStartDate && emptyEndDate) {
+        return filterValue[0] <= columnDate;
+    }
+
+    if (emptyStartDate && !emptyEndDate) {
+        return columnDate <= filterValue[1];
+    }
+
+    return (filterValue[0] <= columnDate) && (columnDate <= filterValue[1]);
+}
+
+export default dateFilter;
