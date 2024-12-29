a0249527c10abf85421afa8065dfe380a1e2477a
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-12-28 23:51:47 -0800

    Update EquipmentTableManager.tsx
    Added the EquipmentCreateForm to the manager for testing.

--- maintenance-tracking-system/src/components/equipment/visuals/EquipmentTableManager.tsx
+++ maintenance-tracking-system/src/components/equipment/visuals/EquipmentTableManager.tsx
@@ -4,7 +4,8 @@ import { Equipment } from "@/lib/equipment/EquipmentInterface";
 import { useState } from "react";
 import EquipmentTable from "./EquipmentTable";
 import { RowSelectionState } from "@tanstack/react-table";
-import BulkStatusUpdateButton from "../buttons/BulkStatusUpdateButton";
+import EquipmentStatusUpdateButton from "@/components/equipment/buttons/EquipmentStatusUpdateButton";
+import EquipmentCreateForm from "../forms/EquipmentCreateForm";
 
 interface ComponentProps {
     initialData: Equipment[];
@@ -17,13 +18,19 @@ const EquipmentTableManager: React.FC<ComponentProps> = ({ initialData }) => {
     return (
         <div className="flex flex-col p-2 gap-2">
             <div>
-                <BulkStatusUpdateButton
+                <EquipmentStatusUpdateButton
                     className="bg-white/50 p-1 rounded-lg"
                     allRows={data}
                     setRows={setData}
                     selectedRows={selectedRows}
                 /> 
             </div>
+
+            <EquipmentCreateForm
+                allRows={data}
+                setRows={setData}
+                onClose={null}
+            />
             
             <EquipmentTable
                 equipmentArray={data}
