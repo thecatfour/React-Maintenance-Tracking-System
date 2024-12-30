dd5c4657c58c02bad131b95244fcde9b21fc29dc
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-12-29 17:37:37 -0800

    Create EquipmentEditOneButton.tsx
    Created a button that displays a form to edit an existing equipment. Also displays an error message in a modal if exactly one row is not selected.

---
+++ maintenance-tracking-system/src/components/equipment/buttons/EquipmentEditOneButton.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import DialogModal from "@/components/generics/visuals/DialogModal";
+import { Equipment } from "@/lib/equipment/EquipmentInterface";
+import { RowSelectionState } from "@tanstack/react-table";
+import { Dispatch, useEffect, useState } from "react";
+import EquipmentFullForm from "../forms/EquipmentFullForm";
+import ConfirmAndCancel from "@/components/generics/buttons/ConfirmAndCancel";
+
+interface ComponentProps {
+    className: string;
+    allRows: Equipment[];
+    setRows: Dispatch<Equipment[]>;
+    selectedRows: RowSelectionState;
+}
+
+const EquipmentEditOneButton: React.FC<ComponentProps> = ({ className, allRows, setRows, selectedRows }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [canOpen, setCanOpen] = useState(false);
+    const [isWarningOpen, setIsWarningOpen] = useState(false);
+    const [selectedRow, setSelectedRow] = useState<Equipment>();
+
+    const tryOpeningForm = () => {
+        if (canOpen) {
+            setIsOpen(true);
+        } else {
+            console.log("warm")
+            setIsWarningOpen(true);
+        }
+    }
+
+    useEffect(() => {
+        if (Object.keys(selectedRows).length === 1) {
+            setCanOpen(true);
+            const selectedId = Object.keys(selectedRows)[0];
+            setSelectedRow(allRows.find((row) => row.id === selectedId))
+        } else {
+            setCanOpen(false);
+            setSelectedRow(undefined);
+        }
+    }, [selectedRows]);
+   
+    return (
+        <>
+            <button
+                onClick={tryOpeningForm}
+                className={className}
+            >
+                Edit One Equipment
+            </button>
+
+            <DialogModal
+                open={isWarningOpen}
+                onClose={setIsWarningOpen}
+            >
+                <div className="flex flex-col gap-2">
+                    There must be exactly one row selected for the "Edit One Equipment" function.
+                    <ConfirmAndCancel
+                        onConfirm={() => setIsWarningOpen(false)}
+                        onClose={setIsWarningOpen}
+                    />
+                </div>
+            </DialogModal>
+
+            <DialogModal
+                open={isOpen}
+                onClose={setIsOpen}
+            >
+                <EquipmentFullForm
+                    allRows={allRows}
+                    setRows={setRows}
+                    onClose={setIsOpen}
+                    selectedRow={selectedRow}
+                />
+            </DialogModal>
+        </>
+    );
+}
+
+export default EquipmentEditOneButton;
