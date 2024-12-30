6b7b4c7802fb47bda4de190bcb3d91107ad6caf0
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-12-29 17:25:16 -0800

    Create ConfirmAndCancel.tsx
    Created a component that displays a confirm button and a cancel button.

---
+++ maintenance-tracking-system/src/components/generics/buttons/ConfirmAndCancel.tsx
@@ -0,0 +1,25 @@
+
+interface ComponentProps {
+    onConfirm: () => void;
+    onClose: (value: boolean) => void;
+}
+
+const ConfirmAndCancel: React.FC<ComponentProps> = ({ onConfirm, onClose }) => {
+    const onCancel = (event: any) => {
+        event.preventDefault();
+        onClose(false);
+    }
+
+    return (
+        <div className="flex justify-between">
+            <button onClick={onConfirm} className="bg-green-700 hover:bg-green-800 px-1 py-0.5 rounded-lg">
+                Confirm
+            </button>
+            <button onClick={onCancel} className="bg-red-700 hover:bg-red-800 px-1 py-0.5 rounded-lg">
+                Cancel
+            </button>
+        </div>
+    );
+}
+
+export default ConfirmAndCancel;
