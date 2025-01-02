ba9ecfa66004567b5c6c179861f5481b30144688
Leslie Atterbury <atterbur@unlv.nevada.edu>
2025-01-01 23:59:05 -0800

    Create MaintenanceRecordTableManager.tsx
    Created a component that will be displayed on the home page to show maintenance records to the user.

---
+++ maintenance-tracking-system/src/components/maintenance-records/visuals/MaintenanceRecordTableManager.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Equipment } from "@/lib/equipment/EquipmentInterface";
+import { MaintenanceRecord } from "@/lib/maintenance-records/MaintenanceRecordInterface";
+import { Dispatch, useState } from "react";
+import MaintenanceRecordTable from "./MaintenanceRecordTable";
+import { RowSelectionState } from "@tanstack/react-table";
+import MaintenanceRecordFullForm from "../forms/MaintenanceRecordFullForm";
+import MaintenanceRecordCreateButton from "../buttons/MaintenanceRecordCreateButton";
+
+interface ComponentProps {
+    equipment: Equipment[];
+    data: MaintenanceRecord[];
+    setData: Dispatch<MaintenanceRecord[]>;
+}
+
+const MaintenanceRecordTableManager: React.FC<ComponentProps> = ({ equipment, data, setData }) => {
+    const [selectedRows, setSelectedRows] = useState<RowSelectionState>({});
+
+    return (
+        <div className="flex flex-col gap-2">
+            <div className="flex gap-2 mt-2 ml-2">
+                <MaintenanceRecordCreateButton
+                    className="bg-zinc-600 hover:bg-zinc-500 p-1 rounded-lg"
+                    allRows={data}
+                    setRows={setData}
+                    equipmentArray={equipment}
+                />
+            </div>
+
+            <MaintenanceRecordTable
+                equipmentArray={equipment}
+                mRecordsArray={data}
+                setSelectedRows={setSelectedRows}
+            />
+        </div>
+    )
+}
+
+export default MaintenanceRecordTableManager;
