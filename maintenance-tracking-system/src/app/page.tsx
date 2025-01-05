1f695ffbae4c68f3614f411991cfbef2a6f265f0
Leslie Atterbury <atterbur@unlv.nevada.edu>
2025-01-04 16:36:15 -0800

    Completed home page dashboard
    The bar chart and recent maintenance records table have been added to the page. To get recent records, a useEffect is used to change the state of an array that holds records with a date within a week of the current day.

--- maintenance-tracking-system/src/app/page.tsx
+++ maintenance-tracking-system/src/app/page.tsx
@@ -3,22 +3,35 @@
 import { EXAMPLE_EQUIPMENT, EXAMPLE_MAINTENANCE_RECORD } from "@/lib/ExampleObjects";
 import EquipmentTableManager from "@/components/equipment/visuals/EquipmentTableManager";
 import Navbar from "@/components/generics/visuals/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Equipment } from "@/lib/equipment/EquipmentInterface";
 import { MaintenanceRecord } from "@/lib/maintenance-records/MaintenanceRecordInterface";
-import { RowSelectionState } from "@tanstack/react-table";
 import MaintenanceRecordTableManager from "@/components/maintenance-records/visuals/MaintenanceRecordTableManager";
 import EquipmentStatusPieChart from "@/components/equipment/visuals/EquipmentStatusPieChart";
+import MaintenanceHoursBarChart from "@/components/maintenance-records/visuals/MaintenanceHoursBarChart";
+import MaintenanceRecordTable from "@/components/maintenance-records/visuals/MaintenanceRecordTable";
+import { RowSelectionState } from "@tanstack/react-table";
 
 export default function Home() {
     const [equipment, setEquipment] = useState<Equipment[]>(EXAMPLE_EQUIPMENT);
+    const [recentMRecords, setRecentMRecords] = useState<MaintenanceRecord[]>([]);
     const [mRecords, setMRecords] = useState<MaintenanceRecord[]>(EXAMPLE_MAINTENANCE_RECORD);
-
+    
     const [isDashboardOpen, setIsDashboardOpen] = useState(false);
     const [isEquipmentOpen, setIsEquipmentOpen] = useState(false);
     const [isMaintenananceRecordsOpen, setIsMaintenanceRecordsOpen] = useState(false);
 
     const [selectedRows, setSelectedRows] = useState<RowSelectionState>({});
+
+    useEffect(() => {
+        let oneWeekAgo = new Date();
+        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+        setRecentMRecords(mRecords.filter(
+            (record) => oneWeekAgo.getTime() <= record.date.getTime()
+        ));
+
+    }, [mRecords])
 
     function focusDashboard() {
         setIsEquipmentOpen(false);
@@ -59,9 +72,27 @@ export default function Home() {
                 />
             }
             {isDashboardOpen &&
-                <EquipmentStatusPieChart
-                    equipmentArray={equipment}
-                />
+                <div className="flex flex-col gap-2">
+                    <div className="bg-white flex flex-row w-full">
+                        <EquipmentStatusPieChart
+                            equipmentArray={equipment}
+                        />
+                        <MaintenanceHoursBarChart
+                            equipmentArray={equipment}
+                            mRecordsArray={mRecords}
+                        />
+                    </div>
+                    <div className="w-full overflow-x-auto">
+                        <MaintenanceRecordTable
+                            mRecordsArray={recentMRecords}
+                            equipmentArray={equipment}
+                            setSelectedRows={setSelectedRows}
+                            title="Recent Maintenance Records"
+                        />
+                    </div>
+                    
+                </div>
+
             }
         </div>  
     );
