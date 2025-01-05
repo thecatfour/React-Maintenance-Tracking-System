f978570e43a79cd6240191b163b430f7cab7ea3e
Leslie Atterbury <atterbur@unlv.nevada.edu>
2025-01-05 12:31:04 -0800

    Update page.tsx
    Changed how the charts are rendered. They now get a scroll bar if the screen is too small to fully render them.

--- maintenance-tracking-system/src/app/page.tsx
+++ maintenance-tracking-system/src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
             }
             {isDashboardOpen &&
                 <div className="flex flex-col gap-2">
-                    <div className="bg-white flex flex-row w-full">
+                    <div className="bg-white flex flex-row w-full overflow-x-auto">
                         <EquipmentStatusPieChart
                             equipmentArray={equipment}
                         />
